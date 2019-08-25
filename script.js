548a5f6a7e921f963659da988663d5856dc24f2c
Dmitriy <aua008@gmail.com>
2019-08-25 15:18:44 +0300

    add stict

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+"use strict";
+
 let money = prompt("Ваш бюджет на месяц?", ""),
     time = prompt("Введите дату в формате YYYY-MM-DD", "");
 
-    
+
 let appData = {
     budget: money,
     timeData: time,
